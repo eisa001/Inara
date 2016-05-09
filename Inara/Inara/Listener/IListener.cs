f9794cf454e7b467c4ce7227217025ecb8ad680a
Eyad Isa <eyad.isa@me.com>
2016-05-08 20:00:25 -0500

    b-2 added a listner for input - EI

---
+++ Inara/Inara/Listener/IListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inara.Listener
+{
+    public interface IListener
+    {
+        void listen(string input);
+    }
+}
---
+++ Inara/Inara_Tests/ListenerTests/ListenerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Inara;
+
+namespace Listener.Tests
+{
+    [TestFixture]
+    public class ListenerTest
+    {
+        [Test]
+        public void ShouldListenToInput()
+        {
+            const string INPUT = "Some Input";
+            var result = "";
+
+            var testObject = new Inara.Listener.Listener();
+            result = testObject.listen(INPUT);
+
+            Assert.AreEqual(INPUT, result, "Listens to input");
+        }
+    }
+}
